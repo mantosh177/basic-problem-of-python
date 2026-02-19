08e7e38aaea250f368b8de1a87cf4d7fc9d82ef1
mantosh <kumarmantosh58989@gmail.com>
2026-02-19 23:40:35 +0530

    .

---
+++ problem21.py
@@ -0,0 +1,11 @@
+# wap to take iput from the user and print the avg.
+
+def avg():
+    a =int(input("Enter a number : "))
+    b =int(input("Enter second number : "))
+    c = int(input("Enter third number : "))
+    avg=(a+b+c)/3
+    print(avg)
+avg()
+print("Thank you ")
+avg()
